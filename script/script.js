c7ca1eb452ccbfc0bb6bcd9360730d9fc20e146a
Suvajit <suvajit.c@outlook.com>
2019-08-02 16:32:58 +0530

    fix

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://api.almostsuvajit.xyz/clashperks", true);
+xmlhttp.open("GET", "https://api.almostsuvajit.xyz/clashperk", true);
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
